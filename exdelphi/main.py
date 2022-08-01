4675070679cc5589e7631cd1d8c940c95d48038a
0-k <martin.t.klein@gmail.com>
2022-08-01 21:09:13 +0200

    update client to simplify get_data_sets_for_product

--- exdelphi/main.py
+++ exdelphi/main.py
@@ -1,6 +1,8 @@
 import client
 
-if __name__ == '__main__':
-    client.authorize(username=USERNAME, password=PASSWORD)
-    product_list = client.get_product_list()
-    print(product_list)
+
+if __name__ == "__main__":
+    client.authorize(username="USERNAME", password="PASSWORD")
+    print(client.get_product_list())
+    print(client.get_data_sets_for_product(1))
+    print(client.get_data(1))
