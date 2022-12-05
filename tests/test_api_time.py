7c0ca5a95091f8b4f0e7dcf934f77c21822b5c54
0-k <martin.t.klein@gmail.com>
2022-12-05 17:35:36 +0100

    is this the actual test result?

--- tests/test_api_time.py
+++ tests/test_api_time.py
@@ -12,5 +12,5 @@ class TestApiTime(unittest.TestCase):
 
     def test_for_valid_int_to_datetime(self):
         self.assertEqual(
-            int_to_datetime(0), datetime.datetime(year=1970, month=1, day=1, hour=1)
+            int_to_datetime(0), datetime.datetime(year=1970, month=1, day=1)
         )
